d908d37d05e44b46f909e02b089059ef28cdeb68
nienhou2 <nienhou2@gmail.com>
2013-12-20 14:33:38 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RehjunCom::Application.config.secret_token = '1265ee1b6ddfb6a94891700fc6e24717d9700bf387f7023db247a8535fbb45ac46f356a5343cad768cdd9c92a19e3ce4dd8c2f7ce2034efa0b6a9d37a2a8eb0b'
